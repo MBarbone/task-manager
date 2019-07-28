731607d294818f0a284868eb826f5bfa1c73a16f
Mike <michael.barbone@gmail.com>
2019-07-28 18:40:07 -0400

    All user routes have authenication

--- src/index.js
+++ src/index.js
@@ -11,34 +11,6 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-const jwt = require("jsonwebtoken");
-
-const myFunction = async () => {
-  const token = jwt.sign({ _id: "abc123" }, "qwertysecret", {
-    expiresIn: "7 days"
-  });
-
-  const data = jwt.verify(token, "qwertysecret");
-};
-
-myFunction();
-
-// const bcrypt = require("bcryptjs");
-
-// const myFunction = async () => {
-//   const password = "red12345!";
-//   const hashedPassword = await bcrypt.hash(password, 8);
-
-//   console.log(password);
-//   console.log(hashedPassword);
-
-//   const isMatch = await bcrypt.compare("red12345!", hashedPassword);
-
-//   console.log(isMatch);
-// };
-
-// myFunction();
-
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
--- src/routers/user-routes.js
+++ src/routers/user-routes.js
@@ -61,23 +61,8 @@ router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
-// READ User by ID
-router.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
 // Update User
-router.patch("/users/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password", "age"];
   const isValidUpdate = updates.every(update =>
@@ -89,36 +74,21 @@ router.patch("/users/:id", async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id);
-    updates.forEach(update => (user[update] = req.body[update]));
+    updates.forEach(update => (req.user[update] = req.body[update]));
 
-    await user.save();
+    await req.user.save();
 
-    // const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-    //   new: true,
-    //   runValidators: true
-    // });
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
+    res.send(req.user);
   } catch (e) {
     res.status(400).send();
   }
 });
 
 // Delete User
-router.delete("/users/:id", async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-
-    if (!user) {
-      return res.status(404).send({ error: "User not found" });
-    }
-
-    res.send(user);
+    await req.user.remove();
+    res.send(req.user);
   } catch (e) {
     res.status(500).send();
   }
