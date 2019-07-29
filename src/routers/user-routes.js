29210d088b4cd927ac977d17d8887a23933011a0
Mike <michael.barbone@gmail.com>
2019-07-29 16:12:10 -0400

    add support for image file upload, user to image relationship not yet built

--- src/routers/user-routes.js
+++ src/routers/user-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer");
 const User = require("../models/user");
 const router = express.Router();
 const auth = require("../middleware/auth");
@@ -93,5 +94,30 @@ router.delete("/users/me", auth, async (req, res) => {
     res.status(500).send();
   }
 });
+
+const upload = multer({
+  dest: "avatars",
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.endsWith(".jpg" || ".jpeg" || ".png")) {
+      return cb(new Error("Image must be a JPG, JPEG, or PNG file."));
+    }
+
+    cb(undefined, true);
+  }
+});
+
+router.post(
+  "/users/me/avatar",
+  upload.single("avatar"),
+  (req, res) => {
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 module.exports = router;
