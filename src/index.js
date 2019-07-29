f8489c33bfdb24b3b87dc68cf2ab25d4249088f0
Mike <michael.barbone@gmail.com>
2019-07-29 00:11:45 -0400

    Login page draft

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 require("./db/mongoose");
 const userRouter = require("./routers/user-routes");
 const taskRouter = require("./routers/task-routes");
+const hbs = require("hbs");
+const path = require("path");
 
 const app = express();
 
@@ -10,6 +12,19 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+
+// Set up Handlebars
+app.set("view engine", "hbs");
+const partialsPath = path.join(__dirname, "../views/partials");
+hbs.registerPartials(partialsPath);
+
+// Serve Static Directory
+app.use(express.static(path.join(__dirname, "../public")));
+
+// login or signup
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
