66938338eeb13571ff370d4fb9cbb33cf6e01a5d
Mike <michael.barbone@gmail.com>
2019-07-31 22:21:45 -0400

    Testing for authenticated routes

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,27 @@
 const request = require("supertest");
 const app = require("../src/app");
+const User = require("../src/models/user");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const userOneID = new mongoose.Types.ObjectId();
+
+const userOne = {
+  _id: userOneID,
+  name: "John",
+  email: "John@example.com",
+  password: "JohnPass!",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneID }, process.env.JWT_SECRET)
+    }
+  ]
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
 
 test("Sign up new user", async () => {
   await request(app)
@@ -10,4 +32,54 @@ test("Sign up new user", async () => {
       password: "qwerty123"
     })
     .expect(201);
+});
+
+test("Should login in existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password
+    })
+    .expect(200);
+});
+
+test("Should not login in nonexistent user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: "nonexistent@example.com",
+      password: "fakepass"
+    })
+    .expect(400);
+});
+
+test("Should get user profile", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should not get profile for unauthenticated user", async () => {
+  await request(app)
+    .get("/users/me")
+    .send()
+    .expect(401);
+});
+
+test("Should delete account for user", async () => {
+  await request(app)
+    .delete("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should not delete account for unauthenticated user", async () => {
+  await request(app)
+    .delete("/users/me")
+    .send()
+    .expect(401);
 });
