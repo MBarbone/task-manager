d32ed0a6a62151da56f3aadaafe3273b9c65ab58
Mike <michael.barbone@gmail.com>
2019-08-04 00:45:26 -0400

    Task test written to create, get, and testing delete security

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,68 +1,72 @@
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
-const User = require('../../src/models/user')
-const Task = require('../../src/models/task')
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const User = require("../../src/models/user");
+const Task = require("../../src/models/task");
 
-const userOneId = new mongoose.Types.ObjectId()
+const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
-    _id: userOneId,
-    name: 'Mike',
-    email: 'mike@example.com',
-    password: '56what!!',
-    tokens: [{
-        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
-    }]
-}
+  _id: userOneId,
+  name: "Mike",
+  email: "mike@example.com",
+  password: "56what!!",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }
+  ]
+};
 
-const userTwoId = new mongoose.Types.ObjectId()
+const userTwoId = new mongoose.Types.ObjectId();
 const userTwo = {
-    _id: userTwoId,
-    name: 'Jess',
-    email: 'jess@example.com',
-    password: 'myhouse099@@',
-    tokens: [{
-        token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET)
-    }]
-}
+  _id: userTwoId,
+  name: "Chico",
+  email: "chico@example.com",
+  password: "coolpass",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET)
+    }
+  ]
+};
 
 const taskOne = {
-    _id: new mongoose.Types.ObjectId(),
-    description: 'First task',
-    completed: false,
-    owner: userOne._id
-}
+  _id: new mongoose.Types.ObjectId(),
+  description: "Finish tasks tests",
+  completed: false,
+  author: userOneId
+};
 
 const taskTwo = {
-    _id: new mongoose.Types.ObjectId(),
-    description: 'Second task',
-    completed: true,
-    owner: userOne._id
-}
+  _id: new mongoose.Types.ObjectId(),
+  description: "Start Chat app",
+  completed: true,
+  author: userOneId
+};
 
 const taskThree = {
-    _id: new mongoose.Types.ObjectId(),
-    description: 'Third task',
-    completed: true,
-    owner: userTwo._id
-}
+  _id: new mongoose.Types.ObjectId(),
+  description: "Finish Node Course",
+  completed: true,
+  author: userTwoId
+};
 
-const setupDatabase = async () => {
-    await User.deleteMany()
-    await Task.deleteMany()
-    await new User(userOne).save()
-    await new User(userTwo).save()
-    await new Task(taskOne).save()
-    await new Task(taskTwo).save()
-    await new Task(taskThree).save()
-}
+const setupDB = async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+  await new User(userTwo).save();
+  await Task.deleteMany();
+  await new Task(taskOne).save();
+  await new Task(taskTwo).save();
+  await new Task(taskThree).save();
+};
 
 module.exports = {
-    userOneId,
-    userOne,
-    userTwoId,
-    userTwo,
-    taskOne,
-    taskTwo,
-    taskThree,
-    setupDatabase
-}
+  userOneId,
+  userOne,
+  userTwo,
+  userTwoId,
+  setupDB,
+  taskOne,
+  taskTwo,
+  taskThree
+};
---
+++ tests/task.test.js
@@ -0,0 +1,50 @@
+const request = require("supertest");
+const app = require("../src/app");
+const {
+  userOneId,
+  userOne,
+  userTwo,
+  userTwoId,
+  setupDB,
+  taskOne,
+  taskTwo,
+  taskThree
+} = require("./fixtures/db");
+const Task = require("../src/models/task");
+
+beforeEach(setupDB);
+
+test("should create task", async () => {
+  const response = await request(app)
+    .post("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({ description: "From my test" })
+    .expect(201);
+
+  const task = await Task.findById(response.body._id);
+  expect(task).not.toBeNull();
+  expect(task.completed).toBe(false);
+});
+
+test("should get tasks", async () => {
+  const response = await request(app)
+    .get("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.length).toEqual(2);
+});
+
+test("Test delete security, User Two should not be able to delete User One task", async () => {
+  const response = await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userTwo.tokens[0].token}`)
+    .send({
+      _id: taskOne._id
+    })
+    .expect(404);
+
+  const task = await Task.findById(taskOne._id);
+  expect(task).not.toBeNull();
+});
